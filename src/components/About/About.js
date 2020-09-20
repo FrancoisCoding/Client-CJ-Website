1abda39a02cb95b9d4ca653bcd30894b6e9fc061
isaiah francois <francoisisaiaht@yahoo.com>
2020-09-20 13:34:51 -0400

    Made navbar into a seperate component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,45 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import { useHistory } from "react-router-dom";
-import { slide as Menu } from "react-burger-menu";
+import Navbar from "../Navbar/Navbar";
 import "./About.css";
 
 const About = () => {
-  let history = useHistory();
-  const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
-      <div className="navbar">
-        <div className="menu">
-          <Menu
-            customBurgerIcon={<img src="https://i.imgur.com/M1SyJcd.png" />}
-            width={"100%"}
-            onOpen={() => setIsOpen(true)}
-            onClose={() => setIsOpen(false)}
-            isOpen={isOpen}
-          >
-            <Link to="/" id="home" className="menu-item-home menu-item">
-              HOME
-            </Link>
-            <Link to="/about" id="about" className="menu-item">
-              ABOUT US
-            </Link>
-            <Link to="/contact" id="contact" className=" menu-item">
-              CONTACT US
-            </Link>
-            <Link to="/faqs" id="faqs" className="menu-item">
-              FAQS
-            </Link>
-          </Menu>
-        </div>
-        <div className="logo-container">
-          <img
-            src="https://i.imgur.com/EjSBPco.png"
-            alt="Quadruple Logo"
-            className="logo-white"
-          />
-        </div>
-      </div>
+      <Navbar />
 
       <section className="section-about">
         <div className="bio-left">
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+import { slide as Menu } from "react-burger-menu";
+import { useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  let history = useHistory();
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="navbar">
+      <div className="menu">
+        <Menu
+          customBurgerIcon={<img src="https://i.imgur.com/M1SyJcd.png" />}
+          width={"100%"}
+          onOpen={() => setIsOpen(true)}
+          onClose={() => setIsOpen(false)}
+          isOpen={isOpen}
+        >
+          <Link to="/" id="home" className="menu-item-home menu-item">
+            HOME
+          </Link>
+          <Link to="/about" id="about" className="menu-item">
+            ABOUT US
+          </Link>
+          <Link to="/contact" id="contact" className=" menu-item">
+            CONTACT US
+          </Link>
+          <Link to="/faqs" id="faqs" className="menu-item">
+            FAQS
+          </Link>
+        </Menu>
+      </div>
+      <div className="logo-container">
+        <img
+          src="https://i.imgur.com/EjSBPco.png"
+          alt="Quadruple Logo"
+          className="logo-white"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
